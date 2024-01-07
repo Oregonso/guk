63195429cabbeb3d090e3b02df5fa52a635dc48d
Oregonso <153789701+Oregonso@users.noreply.github.com>
2024-01-07 22:12:44 +0000

    Update fwd.php

--- gld/fwd.php
+++ gld/fwd.php
@@ -1,6 +1,6 @@
 quantity = 3.9
 itemno = 56711
-kiiod = 2552
+kiiod = 2552.76
 price = 49.95
 myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
 print(myorder.format(quantity, itemno, price))
