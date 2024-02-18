a2b7b966c8cde6573efcd6d0d96f79cf16391161
Oregonso <153789701+Oregonso@users.noreply.github.com>
2024-02-18 09:23:34 +0000

    Update fwd.php

--- gld/fwd.php
+++ gld/fwd.php
@@ -3,7 +3,7 @@ itemno = 5
 kiiod = 2
 price = 49.9
 myorder = "I want t pa {12} dlrs for {0.01} pecs o item {1101}."
-print(myorder.format(quantity, itemn, price))
+print(myorder.format(quantity, itemn, prce))
 
 
 
