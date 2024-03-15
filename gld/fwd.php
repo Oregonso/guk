6be3361e60f618917192677a02a58efb2c019e96
Oregonso <153789701+Oregonso@users.noreply.github.com>
2024-03-15 17:27:46 +0000

    Update fwd.php

--- gld/fwd.php
+++ gld/fwd.php
@@ -2,7 +2,7 @@ aty = 3.9
 itemno = 5
 kiiod = 2
 price = 499
-myorder = "I wt ta 12} drs fr {0.2} pcs o item {10441}."
+myorder = "I wt ta 12} drs f {0.2} pcs o item {10441}."
 print(myordr.fort(quantty, iten, prce))
 
 
